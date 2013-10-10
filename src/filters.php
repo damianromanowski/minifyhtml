4a417ea8e539e28d1bdbd4235ec5cbea381c8d43
Damian Romanowski <damian@deb-admin.com>
2013-10-09 21:23:06 -0400

    Compressed regex and refactored response code

--- src/filters.php
+++ src/filters.php
@@ -17,30 +17,13 @@ App::after(function($request, $response)
     {
         if ($response instanceof Illuminate\Http\Response)
         {
-            $re = '%            # Collapse ws everywhere but in blacklisted elements.
-	            (?>             # Match all whitespans other than single space.
-	              [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
-	            | \s{2,}        # or two or more consecutive-any-whitespace.
-	            ) # Note: The remaining regex consumes no text at all...
-	            (?=             # Ensure we are not in a blacklist tag.
-	              (?:           # Begin (unnecessary) group.
-	                (?:         # Zero or more of...
-	                  [^<]++    # Either one or more non-"<"
-	                | <         # or a < starting a non-blacklist tag.
-	                  (?!/?(?:textarea|pre)\b)
-	                )*+         # (This could be "unroll-the-loop"ified.)
-	              )             # End (unnecessary) group.
-	              (?:           # Begin alternation group.
-	                <           # Either a blacklist start tag.
-	                (?>textarea|pre)\b
-	              | \z          # or end of file.
-	              )             # End alternation group.
-	            )  				# If we made it here, we are not in a blacklist tag.
-	            %ix';
-	        
-        	$output = preg_replace($re, ' ', $response->getOriginalContent());
-
-            $response->setContent($output);
+            $response->setContent(
+            	preg_replace(
+            		'%(?>[^\S]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))%ix',
+            		' ',
+            		$response->getOriginalContent()
+            	)
+            );
         }
     }
 });
