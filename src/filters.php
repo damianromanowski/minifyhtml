9bf83d628da2e01f0d294e4fdc0794ace233af4f
Berend Iwema <berend@inversive.nl>
2013-11-29 09:46:57 +0100

    Originally, this filter called $response->getOriginalContent(). Calling getOriginalContent returns an Illuminate\View\View instance; which already has rendered content available in $response->getContent(). By applying the preg_replace to getOriginalContent(), we invoke the __toString() method of the View instance, which will cause the template to be rendered twice... this means that all PHP code will be executed twice (     including any calls to $user->relation in your view) which makes this a costly operation.
    So instead we should apply the preg_replace to the rendered content of $response->getContent(),
    to avoid duplicate View rendering.

--- src/filters.php
+++ src/filters.php
@@ -21,7 +21,7 @@ App::after(function($request, $response)
             	preg_replace(
             		'%(?>[^\S]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))%ix',
             		' ',
-            		$response->getOriginalContent()
+            		$response->getContent()
             	)
             );
         }
